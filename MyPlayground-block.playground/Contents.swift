5e384205c7c7dccdc1a2eac07e129ff5571e4c9a
lit@pencho.com.cn <lit@pencho.com.cn>
2016-03-14 21:18:15 +0800

    sort array

--- MyPlayground-block.playground/Contents.swift
+++ MyPlayground-block.playground/Contents.swift
@@ -2,6 +2,14 @@
 
 import UIKit
 
+var sortingArr = [5,2,4,1,2]
+sortingArr.sort()
+sortingArr.sort { (a1, a2) -> Bool in
+    return a1 > a2
+}
+
+var comparator = {(a:Int,b: Int) in a < b}
+comparator(1,2)
 
 
 
